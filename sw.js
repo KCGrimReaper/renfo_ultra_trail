3e44d095d91f479cf16057ceeb4543e8898e450e
Benoit <benoit@example.com>
2026-07-27 12:54:30 +0000

    Champ consigne en textarea multi-lignes redimensionnable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'renfo-ultra-trail-v5';
+const CACHE_NAME = 'renfo-ultra-trail-v6';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
