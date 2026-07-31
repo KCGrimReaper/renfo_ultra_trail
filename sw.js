a6ad454808103980f0ad72f8dd431a37b60d04d6
Benoit <benoit@example.com>
2026-07-31 10:59:10 +0000

    Repos synchronisé entre appareils (heure de fin partagée), redirection accueil après séance terminée + anti double-clic, champ charge/détail facultatif

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'renfo-ultra-trail-v8';
+const CACHE_NAME = 'renfo-ultra-trail-v9';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
