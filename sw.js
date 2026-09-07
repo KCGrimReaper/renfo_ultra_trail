466cd6dd99bd78c3ef8b900e7263c60673a06841
Benoit <benoit@example.com>
2026-09-07 15:27:16 +0000

    Player : fond opaque garanti (fix superposition + image de fond restaurée), volet liste coulissant piloté par un seul bouton (croix sidebar retirée)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'renfo-ultra-trail-v17';
+const CACHE_NAME = 'renfo-ultra-trail-v18';
 const APP_SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
